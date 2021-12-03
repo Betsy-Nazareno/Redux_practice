119ff7b05a679ca969affd9ea508284576f1ca67
Betsy Nazareno <betsy.nazareno@devsu.com>
2021-12-03 10:32:48 -0800

    App started

---
+++ redux_app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Botonera from "./components/botonera/botonera";
+
+function App() {
+  return (
+    <div className="App">
+      <Botonera />
+    </div>
+  );
+}
+
+export default App;
---
+++ redux_app/src/components/botonera/botonera.js
@@ -0,0 +1,20 @@
+import "./botonera.css";
+
+const Botonera = () => {
+  return (
+    <div className="Botonera">
+      <header>
+        <div>
+          <h1>Titulo</h1>
+        </div>
+        <ul>
+          <li>Home</li>
+          <li>List</li>
+          <li>New</li>
+        </ul>
+      </header>
+    </div>
+  );
+};
+
+export default Botonera;
